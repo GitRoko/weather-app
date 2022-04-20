5f3ce2415f709a3613774e1c0e9e93f0ba7964f3
Kozhukhovskyi Roman <4kroman@gmail.com>
2022-04-20 20:17:22 +0300

    add geolocation, add getWeather from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,35 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { getWeather } from './api/api';
 
 function App() {
+  const [geolocationUser, setGeolocationUser] = useState(null);
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const location = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        }
+  
+        setGeolocationUser(location);
+      });
+    }
+  }, []);
+
+  // console.log('geolocationUser', geolocationUser);
+
+  useEffect(() => {
+     if (geolocationUser) {
+      getWeather(geolocationUser.longitude, geolocationUser.latitude)
+        .then(data => setWeather(data));
+    }
+  }, [geolocationUser]);
+
+  console.log('weather', weather);
+
   return (
     <div className="App">
       <h1>Weather app</h1>
